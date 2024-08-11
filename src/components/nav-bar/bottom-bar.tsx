65fec13662ec426ece8f12c9288f9bf172b70710
sokebat <sokebat01@gmail.com>
2024-08-11 12:26:58 +0545

    wip:added navigation in navbar

--- src/components/nav-bar/bottom-bar.tsx
+++ src/components/nav-bar/bottom-bar.tsx
@@ -1,18 +1,94 @@
 import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { bottomNavigation } from "@/data/navigation";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const BottomBar = () => {
   return (
-    <div className="bg-green-700">
-      <section className=" max-w-[1400px] mx-auto py-3 px-4 flex justify-between items-center sticky top-0 ">
-        <p>Home</p>
-        <p>About Us</p>
-        <p>Services</p>
-        <p>Our Achivements</p>
-        <p>Gallery</p>
-        <p> Downloads</p>
-        <p>News And Notices</p>
-        <p>Report</p>
-        <p> Contact Us</p>
+    <div className="bg-green-700 w-full">
+      <section className="max-w-[1400px] mx-auto px-4 py-2">
+        <NavigationMenu className="w-full">
+          <NavigationMenuList className="flex w-full justify-between">
+            {bottomNavigation.map((item, index) => {
+              const hasChildren = !!item.children?.length;
+
+              if (hasChildren) {
+                return (
+                  <NavigationMenuItem key={index} className="relative">
+                    <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+                    <NavigationMenuContent
+                      className="p-4 w-96 absolute top-full rounded-md"
+                      style={{
+                        background: "radial-gradient(#32488A, #1d2e61)",
+                      }}
+                    >
+                      {item.children?.map((child, childIndex) => {
+                        const hasGrandChildren = !!child.children?.length;
+
+                        if (hasGrandChildren) {
+                          return (
+                            <Accordion
+                              type="single"
+                              collapsible
+                              key={childIndex}
+                            >
+                              <AccordionItem value="item-1">
+                                <AccordionTrigger>
+                                  {child.title}
+                                </AccordionTrigger>
+                                <AccordionContent>
+                                  {child.children?.map(
+                                    (grandChild, grandChildIndex) => (
+                                      <p key={grandChildIndex}>
+                                        {grandChild.title}
+                                      </p>
+                                    )
+                                  )}
+                                </AccordionContent>
+                              </AccordionItem>
+                            </Accordion>
+                          );
+                        }
+
+                        return (
+                          <NavigationMenuItem key={childIndex} className="w-96">
+                            <NavigationMenuLink
+                              href={child.link}
+                              className="text-center"
+                            >
+                              {child.title}
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        );
+                      })}
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                );
+              }
+
+              return (
+                <NavigationMenuItem key={item.id} className="">
+                  <NavigationMenuLink href={item.link}>
+                    {item.title}
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              );
+            })}
+          </NavigationMenuList>
+        </NavigationMenu>
       </section>
     </div>
   );
---
+++ src/data/navigation.ts
@@ -0,0 +1,185 @@
+export const bottomNavigation = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 4,
+    title: "Our Achievements",
+    link: "/our-achievements",
+  },
+  {
+    id: 2,
+    title: "About Us",
+    link: "/about",
+    children: [
+      {
+        id: 1,
+        title: "Goals and Objectives",
+        link: "/about/goals-and-objectives",
+      },
+      {
+        id: 2,
+        title: "Our Team",
+        link: "/about/our-team",
+        children: [
+          {
+            id: 1,
+            title: "Board of Directors",
+            link: "/about/our-team/board-of-directors",
+          },
+          {
+            id: 2,
+            title: "Account Supervision Committee",
+            link: "/about/our-team/account-supervision-committee",
+          },
+          {
+            id: 3,
+            title: "Advisory Council",
+            link: "/about/our-team/advisory-council",
+          },
+          {
+            id: 4,
+            title: "Top Management",
+            link: "/about/our-team/top-management",
+          },
+          {
+            id: 5,
+            title: "Staff Members",
+            link: "/about/our-team/staff-members",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Services",
+    link: "/services",
+    children: [
+      {
+        id: 1,
+        title: "Saving",
+        link: "/services/saving",
+      },
+      {
+        id: 2,
+        title: "Loan",
+        link: "/services/loan",
+      },
+      {
+        id: 3,
+        title: "Remittance",
+        link: "/services/remittance",
+      },
+      {
+        id: 4,
+        title: "Royal Smart Banking",
+        link: "/services/royal-smart-banking",
+      },
+      {
+        id: 5,
+        title: "SCT Debit Card",
+        link: "/services/sct-debit-card",
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    title: "Gallery",
+    link: "/gallery",
+    children: [
+      {
+        id: 1,
+        title: "Outstanding Members",
+        link: "/gallery/outstanding-members",
+      },
+      {
+        id: 2,
+        title: "Outstanding Employees",
+        link: "/gallery/outstanding-employees",
+      },
+      {
+        id: 3,
+        title: "Events",
+        link: "/gallery/events",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Downloads",
+    link: "/downloads",
+    children: [
+      {
+        id: 1,
+        title: "Account Opening Form",
+        link: "/downloads/account-opening-form",
+      },
+      {
+        id: 2,
+        title: "Optional Account Opening Form",
+        link: "/downloads/optional-account-opening-form",
+      },
+      {
+        id: 3,
+        title: "KYM Form",
+        link: "/downloads/kym-form",
+      },
+      {
+        id: 4,
+        title: "Share Request Form",
+        link: "/downloads/share-request-form",
+      },
+      {
+        id: 5,
+        title: "Organization Account Opening Form",
+        link: "/downloads/organization-account-opening-form",
+      },
+      {
+        id: 6,
+        title: "ATM Application Form",
+        link: "/downloads/atm-application-form",
+      },
+      {
+        id: 7,
+        title: "Mobile Banking Application Form",
+        link: "/downloads/mobile-banking-application-form",
+      },
+      {
+        id: 8,
+        title: "Vacancy Application Form",
+        link: "/downloads/vacancy-application-form",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "News And Notices",
+    link: "/news-and-notices",
+    children: [
+      {
+        id: 1,
+        title: "News",
+        link: "/news-and-notices/news",
+      },
+      {
+        id: 2,
+        title: "Notices",
+        link: "/news-and-notices/notices",
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Reports",
+    link: "/reports",
+  },
+  {
+    id: 9,
+    title: "Contact Us",
+    link: "/contact-us",
+  },
+];
